b285476be8c2b7ba883246f35b34abf4a82b4ea8
AkeksandrF12 <aleksandrsergeev23@list.ru>
2023-04-22 12:54:48 +0300

    add-bookings

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,27 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Value;
+import ru.practicum.shareit.Create;
+import ru.practicum.shareit.Update;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Value
 public class ItemDto {
+    Long id;
+    @NotBlank(groups = {Create.class})
+    @Size(groups = {Create.class, Update.class}, min = 1)
+    String name;
+    @NotBlank(groups = {Create.class})
+    @Size(groups = {Create.class, Update.class}, min = 1)
+    String description;
+    @NotNull(groups = {Create.class})
+    Boolean available;
+    Long ownerId;
+    BookerInfoDto lastBooking;
+    BookerInfoDto nextBooking;
+    List<CommentDto> comments;
 }
