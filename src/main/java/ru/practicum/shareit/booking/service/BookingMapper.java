2fbae957ab2073fb377d8fa41417a7d257a2a6d4
AkeksandrF12 <aleksandrsergeev23@list.ru>
2023-04-30 16:27:28 +0300

    add-bookings

--- src/main/java/ru/practicum/shareit/booking/service/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingMapper.java
@@ -10,7 +10,7 @@ import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
