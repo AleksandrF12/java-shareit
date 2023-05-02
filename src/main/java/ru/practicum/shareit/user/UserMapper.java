cb34b44c1d11b58a6ac220448377f994571b3b3c
AkeksandrF12 <aleksandrsergeev23@list.ru>
2023-05-03 01:00:37 +0300

    add-bookings

--- src/main/java/ru/practicum/shareit/user/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserMapper.java
@@ -1,10 +1,14 @@
 package ru.practicum.shareit.user;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
 @Component
+@Getter
+@Setter
 public class UserMapper {
 
     public UserDto toUserDto(User user) {
